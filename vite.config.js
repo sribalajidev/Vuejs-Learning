fdfa2ac268d38aeb8ac818e9e05652f5a22291dd
Sri <s.sribalaji.dev@gmail.com>
2025-09-25 17:30:14 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/Vuejs-Learning/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
